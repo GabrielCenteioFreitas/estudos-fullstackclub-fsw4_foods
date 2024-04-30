48d853fd9f58173641c691b45b161a4b1f0c6586
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-04-30 02:14:49 -0300

    feat: add product list and product item components

---
+++ app/_helpers/price.ts
@@ -0,0 +1,16 @@
+import { Product } from "@prisma/client";
+
+export const calculateProductTotalPrice = (product: Product): number => {
+  if (product.discountPercentage === 0) {
+    return Number(product.price);
+  }
+
+  return Number(product.price) * (1 - product.discountPercentage / 100);
+};
+
+export const formatCurrency = (value: number): string => {
+  return `R$${Intl.NumberFormat("pt-BR", {
+    currency: "BRL",
+    minimumFractionDigits: 2,
+  }).format(value)}`;
+};

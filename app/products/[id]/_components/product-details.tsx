6d452bbd261c3de6bbd43d394e8bdb74dadad437
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-05-01 15:26:59 -0300

    feat: add cart button to product page

--- app/products/[id]/_components/product-details.tsx
+++ app/products/[id]/_components/product-details.tsx
@@ -136,6 +136,10 @@ const ProductDetails = ({
         <h3 className="px-5 font-semibold">{product.category.name}</h3>
         <ProductList products={complementaryProducts} />
       </div>
+
+      <div className="mt-6 px-5">
+        <Button className="w-full font-semibold">Adicionar à sacola</Button>
+      </div>
     </div>
   );
 };

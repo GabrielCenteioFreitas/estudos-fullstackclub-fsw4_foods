9d4ba11f47935c83b809e4dd0ce647edb33c307c
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-05-04 17:06:43 -0300

    chore: add next auth setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { CartProvider } from "./_context/cart";
+import AuthProvider from "./_providers/auth";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -34,7 +35,9 @@ export default function RootLayout({
         </head>
 
         <body className={inter.className}>
-          <CartProvider>{children}</CartProvider>
+          <AuthProvider>
+            <CartProvider>{children}</CartProvider>
+          </AuthProvider>
         </body>
       </html>
     </>
